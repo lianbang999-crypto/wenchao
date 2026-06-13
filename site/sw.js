2f4b64d8cbe247c06eef6cc2b02d3f77a3f3bbd1
foyue <lianbang999@qq.com>
2026-06-13 19:52:10 +0800

    Revert "Add reader notes and sharing tools"
    This reverts commit 2d92ae51f8238026b735d54369785323860a9db8.

--- site/sw.js
+++ site/sw.js
@@ -1,7 +1,7 @@
 /* 离线缓存：一律网络优先、写穿缓存；离线时回退缓存。
    读过的篇目离线可重读，数据更新后在线立即生效。 */
-const VER = 'wc-v4';
-const SHELL = ['./', 'index.html', 'css/app.css', 'js/app.js', 'js/share.js', 'js/qrcode.js', 'config.js', 'icon.svg', 'data/books.json'];
+const VER = 'wc-v3';
+const SHELL = ['./', 'index.html', 'css/app.css', 'js/app.js', 'config.js', 'icon.svg', 'data/books.json'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(VER).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
